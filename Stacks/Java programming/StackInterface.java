0ba5d1da8e488d45930d595036cee586d695df31
fareedaragab <66780788+farida52369@users.noreply.github.com>
2021-06-26 16:10:27 +0200

    Add files via upload

---
+++ Stacks/Java programming/StackInterface.java
@@ -0,0 +1,16 @@
+package fareeda.ragab;
+
+import java.util.EmptyStackException;
+
+public interface StackInterface {
+    public void push(Object element) throws StackOverflowError;
+    public Object pop() throws EmptyStackException;
+    public boolean isEmpty();
+    public boolean isFull();
+    public int size();
+    public Object peek() throws EmptyStackException;
+    public void Display(Object element);
+    public void traverse();
+    public void clear();
+
+}
