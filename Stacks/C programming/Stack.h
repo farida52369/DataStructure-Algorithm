3e5af9a9c53258735103a32cf8a34333b190d9c8
fareedaragab <66780788+farida52369@users.noreply.github.com>
2021-06-26 14:12:05 +0200

    Add files via upload

---
+++ Stacks/C programming/Stack.h
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+
+#define MAXSIZE 100
+#define StackEntry int
+
+/************* For Linked Lists Implementation ****************/
+#define Linked_Stack
+
+/************* If it's Linked List Implementation **************/
+#ifdef Linked_Stack
+
+typedef struct node
+{
+    StackEntry data;
+    struct node *next;
+}Node;
+
+typedef struct stack
+{
+    int size;
+    Node *top;
+}Stack;
+
+/************ If it's Array Based Implementation **************/
+#else
+
+typedef struct stack
+{
+    int top;
+    StackEntry entry[MAXSIZE];
+} Stack;
+
+#endif
+
+/****************************************************************
+************************** METHODS FOR STACKS *******************
+*****************************************************************/
+
+void push(StackEntry, Stack *);
+StackEntry pop(Stack *);
+bool isEmpty(Stack *);
+bool isFull(Stack *);
+int sizeStack(Stack *);
+StackEntry Top(Stack *);
+void traverse(Stack *, void (*) (StackEntry));
+void clearStack(Stack *);
+void initializeStack(Stack *);
+
