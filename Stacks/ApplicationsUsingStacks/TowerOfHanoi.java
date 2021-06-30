23f7bebfefe883d0ca48452420adce53e26d129d
fareedaragab <66780788+farida52369@users.noreply.github.com>
2021-07-01 01:46:39 +0200

    Update TowerOfHanoi.java

--- Stacks/ApplicationsUsingStacks/TowerOfHanoi.java
+++ Stacks/ApplicationsUsingStacks/TowerOfHanoi.java
@@ -10,7 +10,10 @@ It consists of three rods and a number of disks of different diameters, which ca
 THE RULES:
 Only one disk may be moved at a time.
 Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or an empty rod.
-No disk may be placed on top of a disk that is smaller than it.
+No disk may be placed on top of a disk that is smaller than it..
+
+Space Complexity: The height of the binary tree = N
+Time Complexity: 2^N - 1 = O(2^N)
 
 */
 
