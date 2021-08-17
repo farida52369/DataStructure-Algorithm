98bece678e24a9a5d7f037581399594b1408be1c
fareedaragab <66780788+farida52369@users.noreply.github.com>
2021-08-17 21:05:12 +0200

    Update GCD_LCM.cpp

--- Number Theory/GCD_LCM.cpp
+++ Number Theory/GCD_LCM.cpp
@@ -1,1 +1,48 @@
 
+#include <bits/stdc++.h>
+
+/*
+ * GCD -- Greatest Common Divisor
+ * LARGEST INTEGER WHICH CAN PERFECTLY DIVIDE TWO INTEGERS
+ * LCM -- Least Common Multiple
+ * THE SMALLEST NUMBER WHICH CAN BE DIVIDED BY BOTH NUMBERS
+ *
+ * Khan Academy has great resource to know more about the algorithm of
+ * EUCLIDEAN:
+ * https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+ *
+ */
+
+/*
+ * APPLING EUCLIDEAN ALGORITHM USING RECURSION
+ * A SHOULD BE GREATER THAN b IN THE FIRST CALL AND IT WILL REMAIN THAT WAY
+ * A AND b COULD BE POSITIVE OR NEGATIVE
+ */
+int GCD(int a, int b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/*
+ * FOR TWO NUMBERS A AND B
+ * A * B = GCD(A, B) * LCM(A, B)
+ * LCM(A, B) = (A * B) / GCD(A, B)
+ */
+int LCM(int a, int b) { return (a * b) / GCD(a, b); }
+
+// APPLING EUCLIDEAN ALGORITHM USING LOOPS
+int GCD2(int a, int b) {
+  while (b != 0) {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+
+  std::cout << "GCD: " << GCD(a, b) << "\n";
+  std::cout << "LCM: " << LCM(a, b) << "\n";
+}
+
+// The End :)))
