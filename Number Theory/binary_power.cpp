bdc9b52c40057e41f0f3c0e7fa97c18a4718781c
fareedaragab <66780788+farida52369@users.noreply.github.com>
2021-10-09 10:51:32 +0200

    Update binary_power.cpp

--- Number Theory/binary_power.cpp
+++ Number Theory/binary_power.cpp
@@ -16,9 +16,9 @@ LL binary_pow(LL a, LL b) {
 }
 
 // Using Recursion
-LL binary_pow2(LL a, LL b) {
+LL binary_pow_recursion(LL a, LL b) {
   if (b == 0) return 1;
-  LL res = binary_pow2(a, b / 2);
+  LL res = binary_pow_recursion(a, b / 2);
   if (res % 2)
     return res = res * res * a;
   else
@@ -30,5 +30,5 @@ int main() {
   std::cout << "Enter The a & b: ";
   std::cin >> a >> b;
   std::cout << "a^b Using Loops: " << binary_pow(a, b) << "\n";
-  std::cout << "a^b using Recursion: " << binary_pow2(a, b) << "\n";
+  std::cout << "a^b using Recursion: " << binary_pow_recursion(a, b) << "\n";
 }
