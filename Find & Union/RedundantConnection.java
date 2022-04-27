a3194730ecf30add0787047c9481617a7ad3497a
fareedaragab <66780788+farida52369@users.noreply.github.com>
2022-04-27 23:26:41 +0200

    problems on Find & Union DS

---
+++ Find & Union/RedundantConnection.java
@@ -0,0 +1,23 @@
+
+// USING FIND & UNION DS IN SOME APPLICATIONS
+
+public class RedundantConnection {
+
+    public int[] findRedundantConnection(int[][] edges) {
+        // Nodes from (1 -> N) inclusive
+        FindUnion findUnion = new FindUnion(edges.length + 1);
+
+        int[] res = new int[2];
+        for (int[] edge : edges) {
+            if (!findUnion.union(edge[0], edge[1])) {
+                res[0] = edge[0];
+                res[1] = edge[1];
+                break;
+            }
+        }
+        return res;
+    }
+}
+
+// Problem Link: https://leetcode.com/problems/redundant-connection/
+// This Solution is 100% faster
